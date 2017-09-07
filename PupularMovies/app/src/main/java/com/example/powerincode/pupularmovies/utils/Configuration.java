fc19ed9a0f15ef0d9dfafb0dd327e59aa2866941
Ярослав <powerman23rus@gmail.com>
2017-09-07 15:07:35 +0300

    init project

---
+++ PupularMovies/app/src/main/java/com/example/powerincode/pupularmovies/utils/Configuration.java
@@ -0,0 +1,12 @@
+package com.example.powerincode.pupularmovies.utils;
+
+/**
+ * Created by powerman23rus on 07.09.17.
+ * Enjoy ;)
+ */
+
+public final class Configuration {
+    public final static class API {
+        public static final String MOVIE_DB_API_KEY = "";
+    }
+}
